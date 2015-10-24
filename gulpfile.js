b3bf79605e5be07bb137a403ca914bc0cc43e0ee
Evgeniy Lazarev <me@eugene-lazarev.ru>
2015-10-24 23:15:25 +0300

    Skin CSS example

--- gulpfile.js
+++ gulpfile.js
@@ -2,16 +2,23 @@ var gulp = require('gulp');
 
 var uglify = require('gulp-uglify');
 var cssmin = require('gulp-cssmin');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('uglify', function () {
     gulp.src(['src/jouele.js'])
         .pipe(uglify())
         .pipe(gulp.dest('dist'))
 });
-gulp.task('cssmin', function () {
-    gulp.src(['src/jouele.css'])
+gulp.task('concat', function () {
+    gulp.src(['src/jouele.css', 'src/jouele.skin.css'])
+        .pipe(concatCss('dist/jouele.css'))
+        .pipe(gulp.dest('./'))
         .pipe(cssmin())
+        .pipe(gulp.dest('./'))
+});
+gulp.task('copy', function () {
+    gulp.src(['src/jouele.skin.css'])
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', ['uglify', 'cssmin']);
+gulp.task('default', ['uglify', 'concat', 'copy']);
