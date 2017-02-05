a06ee9922ba1f9d2cf600c6325fcc51a13d7a695
Evgeniy Lazarev <me@eugene-lazarev.ru>
2017-02-06 01:08:14 +0300

    Replacing jPlayer with HowlerJS

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,21 @@
 var gulp = require('gulp');
 
+var concat = require("gulp-concat");
 var uglify = require('gulp-uglify');
 var cleanCSS = require('gulp-clean-css');
 var concatCss = require('gulp-concat-css');
 
-gulp.task('uglify', function () {
-    gulp.src(['src/jouele.js'])
+gulp.task('concatJs', function() {
+    gulp.src(['dist/dependencies/howler.js-2.0.2/howler.core.js', 'src/jouele.js'])
+        .pipe(concat('jouele.js'))
         .pipe(uglify())
         .pipe(gulp.dest('dist'))
 });
-gulp.task('concat', function () {
+gulp.task('concatCss', function () {
     gulp.src(['src/jouele.css', 'src/jouele.skin.css'])
         .pipe(concatCss('dist/jouele.css', {rebaseUrls: false}))
         .pipe(gulp.dest('./'))
-        .pipe(cleanCSS({compatibility: 'ie6'}))
+        .pipe(cleanCSS({compatibility: 'ie8'}))
         .pipe(gulp.dest('./'))
 });
 gulp.task('copy', function () {
@@ -21,4 +23,4 @@ gulp.task('copy', function () {
         .pipe(gulp.dest('dist'))
 });
 
-gulp.task('default', ['uglify', 'concat', 'copy']);
+gulp.task('default', ['concatJs', 'concatCss', 'copy']);
