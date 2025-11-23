9b672ee66a536c44bae48c29d2ab22b743a4c1ce
Evgeny Stepanischev <imbolk@gmail.com>
2025-11-23 20:41:54 +0300

    Opera browser version fix: /OPR\/([0-6].)/ → /OPR\/(\d+)/
    Apply patch automatically during build process via gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,38 @@
 var gulp = require('gulp');
+var fs = require('fs');
+var path = require('path');
 
-var concat = require("gulp-concat");
-var uglify = require('gulp-uglify');
 var cleanCss = require('gulp-clean-css');
+var concat = require("gulp-concat");
 var concatCss = require('gulp-concat-css');
+var uglify = require('gulp-uglify');
 
-gulp.task('concat-css', function () {
-    gulp.src(['src/jouele.css', 'src/jouele.skin.css'])
-        .pipe(concatCss('dist/jouele.css', {rebaseUrls: false}))
-        .pipe(gulp.dest('./'))
+gulp.task('patch-howler', function (done) {
+    var howlerPath = path.join(__dirname, 'node_modules/howler/src/howler.core.js');
+    var content = fs.readFileSync(howlerPath, 'utf8');
+    
+    // Patch Opera browser version regex: /OPR\/([0-6].)/ → /OPR\/(\d+)/
+    // Simple string replacement - replace ([0-6].) with (\d+)
+    var patchedContent = content.replace('([0-6].)', '(\\d+)');
+    
+    if (content === patchedContent) {
+        console.warn('Warning: Howler.js patch was not applied. The file may have already been patched or the pattern changed.');
+    }
+    
+    fs.writeFileSync(howlerPath, patchedContent, 'utf8');
+    done();
 });
-gulp.task('copy-js', function () {
-    gulp.src(['src/jouele.js'])
-        .pipe(gulp.dest('dist'))
-});
+
 gulp.task('minify-css', function () {
-    gulp.src(['src/jouele.css', 'src/jouele.skin.css'])
-        .pipe(concatCss('jouele/jouele.min.css', {rebaseUrls: false}))
-        .pipe(cleanCss({compatibility: 'ie8'}))
-        .pipe(gulp.dest('./'))
+    return gulp.src('src/*.css')
+            .pipe(concatCss('dist/jouele.min.css'))
+            .pipe(cleanCss({compatibility: 'ie8'}))
+            .pipe(gulp.dest('./'))
 });
-gulp.task('uglify-js', function() {
-    gulp.src(['node_modules/howler/src/howler.core.js', 'src/jouele.js'])
-        .pipe(concat('jouele.min.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('jouele'))
-});
-
-gulp.task('default', ['minify-css', 'uglify-js']);
-gulp.task('npm', ['concat-css', 'copy-js']);
+gulp.task('uglify-js', gulp.series('patch-howler', function() {
+    return gulp.src(['node_modules/howler/src/howler.core.js', 'src/jouele.js'])
+            .pipe(concat('jouele.min.js'))
+            .pipe(uglify())
+            .pipe(gulp.dest('dist'))
+}));
+gulp.task('default', gulp.parallel('minify-css', 'uglify-js'));
